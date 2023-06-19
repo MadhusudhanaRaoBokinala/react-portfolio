f16facf326c5491b99c5ed8c7827a5b29f02d09e
MadhusudhanaRaoBokinala <bokinalamadhu@gmail.com>
2023-06-19 12:06:48 +0530

    domain update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div name="home" className=' h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
             <div className=' flex flex-col max-w-screen-lg mx-auto  items-center justify-center h-full px-4 md:flex-row'>
                 <div className='flex flex-col justify-center h-full'>
-                    <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
+                    <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Backend Developer</h2>
                     <p className='text-gray-500 py-4 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio laboriosam sed fugiat doloribus itaque quas ut dolorem sunt est ratione! Repellendus accusamus inventore voluptas vitae porro voluptatibus modi, aspernatur quos.</p>
                     <div>
                         <Link to="portfolio" smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>

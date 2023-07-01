8415913ee5f8ae65464c65842c8e4ca79ebe7cd0
MadhusudhanaRaoBokinala <bokinalamadhu@gmail.com>
2023-07-01 19:27:48 +0530

    bio update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
                 <div className='flex flex-col justify-center h-full'>
                     <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
                     <p className='text-gray-500 py-4 '>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio laboriosam sed fugiat doloribus itaque quas ut dolorem sunt est ratione! Repellendus accusamus inventore voluptas vitae porro voluptatibus modi, aspernatur quos.</p>
+                        Welcome to my portfolio! I'm Madhu, a web developer specializing in engaging web applications using HTML, CSS, JavaScript, React, and Java. I strive to deliver exceptional user experiences through clean code, visually appealing interfaces, and robust functionality. Let's bring your web visions to life!</p>
                     <div>
                         <Link to="portfolio" smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                             Portfolio

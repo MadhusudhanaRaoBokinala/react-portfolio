1ad4446e7d313038c2bda6e415961a086a947f2b
MadhusudhanaRaoBokinala <bokinalamadhu@gmail.com>
2023-07-05 23:00:59 +0530

    git usual commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,11 +4,14 @@ import heroImage from '../assets/HeroImage.png';
 import { Link } from 'react-scroll';
 
 const Home = () => {
+
     return (
         <div name="home" className=' h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
             <div className=' flex flex-col max-w-screen-lg mx-auto  items-center justify-center h-full px-4 md:flex-row'>
-                <div className='flex flex-col justify-center h-full'>
-                    <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
+                <div className='flex flex-col justify-center h-full '>
+                    <div className='h-1/5'>
+                        <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
+                    </div>
                     <p className='text-gray-500 py-4 '>
                         Welcome to my portfolio! I'm Madhu, a web developer specializing in engaging web applications using HTML, CSS, JavaScript, React, and Java. I strive to deliver exceptional user experiences through clean code, visually appealing interfaces, and robust functionality. Let's bring your web visions to life!</p>
                     <div>
